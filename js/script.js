cf87190b0fe01768b5266be6a7871dd0d5d4480f
alexdavidenko02 <123631163+alexdavidenko02@users.noreply.github.com>
2023-07-01 13:46:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+    const menuItem = document.querySelectorAll('.menu_item');
+    const hamburger = document.querySelector('.hamburger');
+    const body = document.querySelector('body');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+        body.style.overflow = menu.classList.contains('menu_active') ? 'hidden' : 'auto';
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('menu_active');
-        })
-    })
-})
+            body.style.overflow = menu.classList.contains('menu_active') ? 'hidden' : 'auto';
+        });
+    });
+});
